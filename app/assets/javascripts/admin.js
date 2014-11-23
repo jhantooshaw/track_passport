6a8ea2adbff4acc4d3992c6522d2221a0b89925b
Jhantoo <jhantoo.shaw@stellingtech.com>
2014-11-23 18:36:37 +0530

    M

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -25,10 +25,8 @@ $(document).on('ready page:load', function() {
         return !(e.which !== 8 && e.which !== 0 &&
                 (e.which < 48 || e.which > 57) && e.which !== 46);
     });
-
     var nowTemp = new Date();
     var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-
     var ex_d = $('.expiry_date').datepicker({
         format: 'dd-mm-yyyy',
         onRender: function(date) {
@@ -36,8 +34,9 @@ $(document).on('ready page:load', function() {
         }
     }).on('changeDate', function(ev) {
         ex_d.hide();
+    }).click(function() {
+        ex_d.show();
     }).data('datepicker');
-
 
     var dob = $('.dob').datepicker({
         format: 'dd-mm-yyyy',
@@ -46,8 +45,9 @@ $(document).on('ready page:load', function() {
         }
     }).on('changeDate', function(ev) {
         dob.hide();
+    }).click(function() {
+        dob.show();
     }).data('datepicker');
-
 });
 
 function responsive_table(pagination) {
