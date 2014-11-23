1d2815476656fcb2916b27383ad0abc630c0baa6
Jhantoo <jhantoo.shaw@stellingtech.com>
2014-11-23 15:56:31 +0530

    M

--- app/controllers/visas_controller.rb
+++ app/controllers/visas_controller.rb
@@ -11,7 +11,7 @@ class VisasController < ApplicationController
           @visa = Visa.joins(:passport).where("visas.unique_number=? and passports.mobile=?", params[:unique_number], params[:mobile]).first
          # @visa = Visa.joins(:passport).where(unique_number: params[:unique_number], mobile: params[:mobile])
         end        
-        raise "Unique number is not found. Enter valid number." if @visa.blank?
+        raise "Either Unique number or registered number is not found. Enter valid number." if @visa.blank?
       rescue Exception => e
         @error = e.message
       end
